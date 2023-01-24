d34bf8c47480b82d578646e6e669ec0c26c991f0
aznyan <aznyan5510@gmail.com>
2023-01-23 19:39:53 -0500

    Changed functions for game to make it run properly and added comments to read the code better. Also made alerts so I can see the outcome when ran on web

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function getComputerChoice () {
     //create a algorithm to get random number for computer
-    let randomNum = Math.floor(Math.random() * 100) + 1;
+    let randomNum = Math.floor(Math.random() * 100);
+
     //assign rock, paper and sciccors for the computer
     if (randomNum < 33) {
         return "rock";
-    } else if (randomNum > 33 && randomNum < 66) {
+    } else if (randomNum < 66) {
         return "paper";
     } else {
         return "scissor";
@@ -14,7 +15,7 @@ function getComputerChoice () {
 //function for player choice
 function getPlayerChoice() {
     let choice = prompt("Rock, Paper, or Scissor?: ", "")
-    choice.toLowerCase;
+    choice = choice.toLowerCase();
     if (choice === "rock") {
         return "rock";
     } else if (choice === "paper") {
@@ -22,41 +23,52 @@ function getPlayerChoice() {
     } else if (choice === "scissor") {
         return "scissor";
     } else {
-        return "that is not an option";
+        return choice + " is not an option";
     }
 } 
 
 //function for the round to see who wins
 function playRound (playerSelection, computerSelection) {
-    
-   playerSelection = getPlayerChoice();
-   computerSelection = getComputerChoice();
+   
+    //gets player choice and computer choice
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
 
+    //All the outcomes for the rock, paper, scissor 
     if (playerSelection === "rock" && computerSelection === "rock") {
+        alert("Draw! Rock vs Rock");
         console.log("Draw! Rock vs Rock");
         return "draw";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+        alert("Lose! Rock lose to Paper");
         console.log("Lose! Rock lose to Paper");
         return "lose";
     } else if (playerSelection === "rock" && computerSelection === "scissor") {
+        alert("Win! Rock beat Scissor");
         console.log("Win! Rock beat Scissor");
         return "win";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        alert("Win! Paper beats Rock");
         console.log("Win! Paper beats Rock");
         return "win";
     } else if (playerSelection === "paper" && computerSelection === "paper") {
+        alert("Draw! Paper vs Paper");
         console.log("Draw! Paper vs Paper");
         return "draw";
     } else if (playerSelection === "paper" && computerSelection === "scissor") {
+        alert("Lose! Paper lose to scissor");
         console.log("Lose! Paper lose to scissor");
         return "lose";
     } else if (playerSelection === "scissor" && computerSelection === "rock") {
+        alert("Lose! Scissor lose to Rock");
         console.log("Lose! Scissor lose to Rock");
         return "lose";
     } else if (playerSelection === "scissor" && computerSelection === "paper") {
+        alert("Win! Scissor beats Paper");
         console.log("Win! Scissor beats Paper");
         return "win";
     } else if (playerSelection == "scissor" && computerSelection === "scissor") {
+        alert("Draw! Scissor vs Scissor");
         console.log("Draw! Scissor vs Scissor");
         return "draw";
     }
@@ -64,20 +76,24 @@ function playRound (playerSelection, computerSelection) {
 
 //function for the game of best to 5
 function game () {
+    let playerWin = 0;
+    let computerWin = 0;
+    let draw = 0;
+
+    //loops 5 times for the game
     for (let i = 0; i < 5; i++) {
-        let playerWin = 0;
-        let computerWin = 0;
-        let draw = 0;
-        let player, computer;
-        if (playRound (player, computer) === "win") {
+        let play = playRound();
+        if (play === "win") {
             playerWin++;
-        } else if (playRound (player, computer) === "lose") {
+        } else if (play === "lose") {
             computerWin++;
         } else {
             draw++;
         }
-        return "You have " + playerWin + " wins, " + computerWin + " loses and " + draw + " draw";
-    } 
+    }
+    alert("You have " + playerWin + " win, " + computerWin + " lose and " + draw + " draw");
+    return "You have " + playerWin + " win, " + computerWin + " lose and " + draw + " draw"; 
 }
 
+//playing the game
 console.log(game());
